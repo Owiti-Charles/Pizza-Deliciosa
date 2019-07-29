2cfe3d1c6afa128316383dd1a87cc8cd0f2a982a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-07-29 11:41:52 +0300

    edit non responsive button

--- js/script.js
+++ js/script.js
@@ -133,13 +133,6 @@ $(document).ready(function(){
         }
         let topping_value = ptopping.length*50;
         console.log("toppins value" + topping_value);
-
-        if((psize == "0") && (pcrust == "0")){
-          console.log("nothing selected");
-        }
-        else{
-            console.log("hello");
-        }
         total = price + crust_price + topping_value;
         console.log(total);
 
